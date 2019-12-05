2740beed89ee6582d686df3d375b64604cdd5857
mfranchi16 <54875560+mfranchi16@users.noreply.github.com>
2019-12-04 23:45:40 -0600

    Add files via upload

---
+++ myLogin.js
@@ -0,0 +1,29 @@
+/*
+	Mario Franchi
+	Web and Dist Programming
+	Fall 2019
+	Final Project
+*/
+
+var counter = 0;
+
+function login(){
+	counter++;
+	if(counter == 3){
+		document.getElementById("log").disabled = true;
+	}
+	
+	var user = document.getElementById("username").value;
+	var pass = document.getElementById("password").value;
+    
+	var boo = false;
+	
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xmlhttp.open("POST", "login.php", true);
+    xmlhttp.send();   
+}
