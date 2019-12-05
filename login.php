a6f79ca345cb47b5dff495bf8998cc789ea70d0a
mfranchi16 <54875560+mfranchi16@users.noreply.github.com>
2019-12-05 00:12:07 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+	$combo = $_POST["pass2"];
+	
+	echo $combo;
+	
+	//Alpha
+	if($combo == 'Alpha FlightKane'){
+		$boo = true;
+		echo $boo;
+	}
+	
+	//Bravo
+	if($combo == 'Bravo FlightToews'){
+		$boo = true;
+		echo $boo;
+	}
+	
+	//Charlie
+	if($enteredUsername == 'Charlie Flight' and $enteredPassword == 'Shaw'){
+		$boo = true;
+		echo $boo;
+	}
+	
+	//Delta
+	if($enteredUsername == 'Delta Flight' and $enteredPassword == 'Strome'){
+		$boo = true;
+		echo $boo;
+	}
+	
+	//POC
+	if($enteredUsername == 'POC' and $enteredPassword == 'Dach'){
+		$boo = true;
+		echo $boo;
+	}
+	
+	//Cadre
+	if($enteredUsername == 'Cadre' and $enteredPassword == 'Saad'){
+		$boo = true;
+		echo $boo;
+	}
+?>
